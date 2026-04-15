c47d985475b1bd858ad8c32f3ec7a297dceba2ec
Jack <jwa34@cam.ac.uk>
2026-04-15 12:04:39 +0100

    Add code for exercise 2.1 constructors, member functions, and header files.

--- exercises/2.1-class-types/complex.cpp
+++ exercises/2.1-class-types/complex.cpp
@@ -9,20 +9,44 @@ Complex::Complex(double real, double imag) : re(real), im(imag) {
 
 double Complex::real() {
   // Return real component
+  return re;
 }
 
 /* Add definition of a member function to access the imaginary component */
+double Complex::imag() {
+  // Return imaginary component
+  return im;
+}
 
 Complex Complex::conj() {
   // Return complex conjugate
+  return Complex{re, -im};
 }
 
 /* Add definition of 'norm' member function. Hint: Look up std::sqrt from the
 cmath header to help calculate the magnitude of a complex number */
+double Complex::norm() {
+  // Return l2 norm component
+  return std::sqrt(re*re + im*im);
+}
 
 /* Add definition of 'add' member function */
+Complex Complex::add(Complex val) {
+  // Return Complex val added to our value
+  return Complex{re + val.re, im + val.im};
+}
 
 bool Complex::equals(Complex other_complex) {
   // Return true if the real and imaginary parts of the complex numbers are
   // equal. False otherwise.
+  if ((re == other_complex.re) && (im == other_complex.im)) {
+    return true;
+  } else {
+    // Strictly we could leave this outside the else as a final return.
+    return false;
+  }
+
+  // Note this could all be simplified to:
+  // return (re == other_complex.re && im == other_complex.im);
+
 }
--- exercises/2.1-class-types/complex.hpp
+++ exercises/2.1-class-types/complex.hpp
@@ -9,10 +9,14 @@ public:
   - A constructor using just a real component
   - A constructor using real and imaginary components
   */
+  Complex() = default;
+  Complex(double re);
+  Complex(double re, double im);
 
   // Access components
   double real();
   /* Add declaration to access the imaginary component */
+  double imag();
 
   // Compute the complex conjugate
   Complex conj();
@@ -20,10 +24,12 @@ public:
   /* Add declaration for member function 'norm' that takes no arguments and
   returns the magnitude of the complex number.
   */
+  double norm();
 
   /* Add declaration for an 'add' member function as so: z = i.add(j)
   I.e. For complex numbers i and j, z is the result of i + j. 
   */
+  Complex add(Complex j);
 
   // Check if two complex numbers are equal
   bool equals(Complex other_complex);
@@ -32,6 +38,9 @@ public:
   the complex number. These should have type 'double' and a suitable default
   value.
   */
+private:
+  double re = 0.0;
+  double im = 0.0;
 };
 
 #endif
