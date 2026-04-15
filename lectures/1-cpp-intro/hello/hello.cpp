aac78e34f4d5c7ea00f855e95aac07fb0a1887a9
Jack <jwa34@cam.ac.uk>
2026-04-15 11:17:25 +0100

    Update lecture 1 example to make hello world take an input and use a function.

--- lectures/1-cpp-intro/hello/hello.cpp
+++ lectures/1-cpp-intro/hello/hello.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+
+void say_hello(std::string name) {
+  std::cout << "Hello, " << name << "!"<< std::endl;
+}
 
 int main(int argc, char* argv[]) {
-  std::cout << "Hello, world!" << std::endl;
+
+  auto name = std::string{};
+
+  std::cout << "Write your name and press return to receive a greeting: ";
+  // Read in from terminal
+  std::cin >> name;
+
+  // Print out hello name using function
+  say_hello(name);
+
   return 0;
 }
