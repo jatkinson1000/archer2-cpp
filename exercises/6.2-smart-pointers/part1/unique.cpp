5ce28396107c90d06b219e40733e754eb531498d
Jack <jwa34@cam.ac.uk>
2026-04-20 08:42:51 +0100

    Add code for exercise 6.2 smart pointers.

--- exercises/6.2-smart-pointers/part1/unique.cpp
+++ exercises/6.2-smart-pointers/part1/unique.cpp
@@ -16,11 +16,16 @@ int main()
     << "------------" << std::endl;
 
   // TODO: Define a unique pointer of A called p1
+  std::unique_ptr<A> p1 = std::make_unique<A>();
 
   // TODO: Call printA()
+  // *p1.print() does not work. We need to explicitly dereference first: (*p1).print()
+  // Shorthand for `(*p1).` is `p1->`
+  p1->printA();
 
   // TODO: Print address of the containing pointer
-
+  // NOTE: We cannot use p1, &p1, 
+  std::cout << p1.get() << std::endl;
 
   std::cout << std::endl
     << "------------" << std::endl
@@ -28,11 +33,15 @@ int main()
     << "------------" << std::endl;
 
   // TODO: Make a new unique pointer of A called p2
+  std::unique_ptr<A> p2 = std::make_unique<A>();
 
   // TODO: Call printA()
+  p2->printA();
 
   // TODO: Print the addresses of p1 and p2
-
+  std::cout << p1.get() << std::endl;
+  std::cout << p2.get() << std::endl;
+  
 
   std::cout << std::endl
     << "-----------" << std::endl
@@ -41,10 +50,21 @@ int main()
 
   // TODO: Set p2 equal to p1
   // What happens when you compile this?
+  // ```console
+  // error: use of deleted function 'std::unique_ptr<_Tp, _Dp>& std::unique_ptr<_Tp, _Dp>::operator=(con
+  // st std::unique_ptr<_Tp, _Dp>&) [with _Tp = A; _Dp = std::default_delete<A>]'
+  //    53 |   p2 = p1;
+  //          |        ^~
+  // ```
+  
+  // p2 = p1;
 
   // TODO: Use move to move p1 into p2 instead, what happens?
+  p2 = std::move(p1);
 
   // TODO: Print the addresses of p1 and p2
+  std::cout << p1.get() << std::endl;
+  std::cout << p2.get() << std::endl;
 
   return 0;
 }
--- exercises/6.2-smart-pointers/part2/shared.cpp
+++ exercises/6.2-smart-pointers/part2/shared.cpp
@@ -16,11 +16,15 @@ int main()
     << "------------" << std::endl;
 
   // TODO: Create a shared pointer of A called p1
+  // This is wrong! shared_ptr is the type! auto p1 = std::shared_ptr<A>();
+  // We could use std::shared_ptr<A> p1 = std::make_shared<A>(); but auto is cleaner:
+  auto p1 = std::make_shared<A>();
 
   // TODO: Call printA()
+  p1->printA();
 
   // TODO: Print address of the containing pointer
-
+  std::cout << p1.get() << std::endl;
 
   std::cout << std::endl
     << "------------" << std::endl
@@ -28,10 +32,14 @@ int main()
     << "------------" << std::endl;
 
   // TODO: Create a new shared pointer p2 that shares ownership of p1
+  auto p2 = p1;
 
   // TODO: Call printA()
+  p2->printA();
 
   // TODO: Print the addresses of p1 and p2
+  std::cout << p1.get() << std::endl;
+  std::cout << p2.get() << std::endl;
 
 
   std::cout << std::endl
@@ -40,13 +48,20 @@ int main()
     << "------------------" << std::endl;
 
   // TODO: Print the use count of p1 and p2
+  std::cout << p1.use_count() << std::endl;
+  std::cout << p2.use_count() << std::endl;
 
   // TODO: Look up how to relinquish ownership of a resource
   // Relinquish ownership of p1
+  p1.reset();
 
   // TODO: Print the use count of p1 and p2
+  std::cout << p1.use_count() << std::endl;
+  std::cout << p2.use_count() << std::endl;
 
   // TODO: Print the addresses of p1 and p2
+  std::cout << p1.get() << std::endl;
+  std::cout << p2.get() << std::endl;
 
   return 0;
 }
